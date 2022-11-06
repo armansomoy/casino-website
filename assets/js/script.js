5b059e39d9751fb6bac6429e9245e5864b5f60a0
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2022-11-06 19:41:22 +0600

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,16 +28,14 @@ $(".casino-slider").slick({
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
+        slidesToScroll: 1,
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 769,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
       },
     },
     {
@@ -64,16 +62,14 @@ $(".partner-slider").slick({
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
+        slidesToScroll: 1,
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 769,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
       },
     },
     {
