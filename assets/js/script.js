af83ae887f704af61332f17d0a73e328665e8ac6
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2022-11-06 19:14:58 +0600

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,56 @@
 $(function () {
-  'use strict';
-
+  "use strict";
 
   // WoW Js
   var wow = new WOW({
     offset: 2,
-    mobile: false
-  })
+    mobile: false,
+  });
   wow.init();
-
 
   //products part
   var containerEl = document.querySelector(".filter");
   var mixer = mixitup(containerEl);
-
 });
 
+$(".casino-slider").slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  arrows: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  nextArrow: ".left-arrow",
+  prevArrow: ".right-arrow",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
 
-
-$('.partner-slider').slick({
+$(".partner-slider").slick({
   dots: false,
   infinite: true,
   slidesToShow: 6,
@@ -26,5 +58,30 @@ $('.partner-slider').slick({
   arrows: false,
   autoplay: true,
   autoplaySpeed: 2000,
-});
 
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
